295924d20743dbcf9b795b5044a85408bbda585d
Felipe Gabriel <felipegabrielodss@gmail.com>
2026-06-02 04:13:29 -0300

    feat: add user, role and password recover entities

---
+++ library/src/main/java/com/library/projections/UserDetailsProjection.java
@@ -0,0 +1,8 @@
+package com.library.projections;
+
+public interface UserDetailsProjection {
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
